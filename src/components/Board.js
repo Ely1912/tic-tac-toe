eebb43cba8e6361e819f657c533ce39a7177cb6e
Elymar <=>
2021-07-16 19:45:16 -0300

    Tic-tac-toe first commit

---
+++ src/components/Board.js
@@ -0,0 +1,11 @@
+import React from 'react';
+//import Board from './Board.css';
+import Square from './Square';
+
+const Board = () =>{
+  return <div className='board-stye'>
+    <Square />
+  </div>
+};
+
+export default Board;
---
+++ src/components/Square.js
@@ -0,0 +1,10 @@
+import React from 'react';
+//import Square from './Square.css';
+
+const Square = () =>{
+  return <div className='square-stye'>
+    <h1>hola</h1>
+  </div>
+};
+
+export default Square;
